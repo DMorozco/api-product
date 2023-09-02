5ecf2f350aab4f121f4c13abfb99f57e4f2dbdb7
DMorozco <131802170+DMorozco@users.noreply.github.com>
2023-09-02 19:24:09 +0200

    feat: added mongodb implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,34 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductService } from './domain/product.service';
 import { ProductController } from './app/product.controller';
+import { ProductService } from './domain/product.service';
 import { MongoRepository } from './infrastructure/mongodb/mongoRepository';
+import { Product, ProductSchema } from './domain/product.model';
 
 @Module({
-  imports: [],
-  controllers: [AppController, ProductController],
+  imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        name: Product.name,
+        useFactory: () => {
+          return ProductSchema;
+        },
+      },
+    ]),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot(),
+  ],
+  controllers: [ProductController],
   providers: [
-    AppService, 
     ProductService, 
     MongoRepository
   ],
--- src/infrastructure/mongodb/mongoRepository.ts
+++ src/infrastructure/mongodb/mongoRepository.ts
@@ -1,33 +1,36 @@
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { ProductInput } from "src/app/product.dto";
 import { Product } from "../../domain/product.model";
 import { IRepository } from "../repository/IRepository";
-import { Injectable } from "@nestjs/common";
-import { ProductInput } from "src/app/product.dto";
 
 @Injectable()
 export class MongoRepository implements IRepository {
+  constructor(@InjectModel(Product.name) private readonly productModel: Model<Product>) {}
 
-  constructor(
-    // Libreria de MongoDB
-  ) {}
-
-  async insert(product: ProductInput): Promise<Product> {
-    return new Product('code', 'product 1', 'desc product 1', 1);
+  async insert(productInput: ProductInput): Promise<Product> {
+    const newProduct = new this.productModel(productInput);
+    return await newProduct.save();
   }
 
-  async update(id: string, product: ProductInput): Promise<Product> {
-    return new Product('code', 'product 1', 'desc product 1', 1);
+  async update(id: string, productInput: ProductInput): Promise<Product> {
+    const updatedProduct = await this.productModel.findByIdAndUpdate(id, productInput, { new: true });
+    return updatedProduct;
   }
 
   async delete(id: string): Promise<boolean> {
-    return true;
+    const result = await this.productModel.deleteOne({ _id: id });
+    return result.deletedCount > 0;
   }
 
-  async getById(id: string): Promise<Product> {
-    return new Product('code', 'product 1', 'desc product 1', 1);
+  async getById(id: string): Promise<Product | null> {
+    const product = await this.productModel.findById(id);
+    return product;
   }
 
   async getAll(): Promise<Product[]> {
-    const p = new Product('code', 'product 1', 'desc product 1', 1);
-    return [p, p, p]
+    const products = await this.productModel.find();
+    return products;
   }
 }
